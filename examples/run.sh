a1d636db4b114a80cacbf7aaea53babc85ba03f5
nicholas.christopher <nicholas.christopher@seleritycorp.com>
2013-05-15 17:25:55 -0400

    Examples

--- examples/run.sh
+++ examples/run.sh
@@ -1,20 +1,35 @@
 #!/bin/sh
 
-case "$1" in
-        argopub)
-          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar -c $2 -d database/application.xml,database/environment.xml -t template/argopub.scotty -o argopub.properties
+TEMPLATE=$1
+CONTEXT=""
+if [ ".$2" != "." ]; then
+    CONTEXT="-c $2"
+fi
+
+case "$TEMPLATE" in
+        print)
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar --print -d database/application.xml,database/environment.xml,database/host.xml,database/amazon.xml,database/datasource.xml
           ;;
         alerts)
-          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar -c $2 -d database/application.xml -t template/alerts.scotty -o alerts.json
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/application.xml -t template/alerts.scotty
+          ;;
+        argopub)
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/application.xml,database/environment.xml,database/datasource.xml -t template/argopub.scotty
+          ;;
+        argosub)
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/application.xml,database/environment.xml,database/datasource.xml -t template/argosub.scotty
           ;;
         aegir)
-          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar -c $2 -d database/application.xml,database/environment.xml -t template/aegir.scotty -o aegir.properties
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/application.xml,database/environment.xml -t template/aegir.scotty
           ;;
         amazon)
-          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar -c $2 -d database/amazon.xml -t template/amazon.scotty -o amazon.conf
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/amazon.xml -t template/amazon.scotty
+          ;;
+        bulkdata)
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/application.xml,database/environment.xml,database/datasource.xml -t template/bulkdata.scotty
           ;;
         universe)
-          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar -d database/host.xml -t template/universe.scotty -o universe.properties
+          java -jar ../target/scotty-1.0-SNAPSHOT-jar-with-dependencies.jar $CONTEXT -d database/host.xml -t template/universe.scotty
           ;;
 esac
 
