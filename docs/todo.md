9f03f88309e26a1bb50eb3503e1bba71f6cca1f3
nicholas.christopher <nicholas.christopher@seleritycorp.com>
2013-06-07 13:46:24 -0400

    Improved import and noted some enhancements

--- docs/todo.md
+++ docs/todo.md
@@ -3,6 +3,8 @@ TODO:
 
 * richer test & examples
 * document adding user Java code to the beanShell scripts
+* enhance include mechanism to have a context
+
 
 
 
--- src/main/java/scotty/database/parser/Utilities.java
+++ src/main/java/scotty/database/parser/Utilities.java
@@ -33,88 +33,88 @@ public final class Utilities {
     private static final Logger LOGGER = Logger.getLogger(Utilities.class.getName());
 
     private Utilities() {
-	}
+    }
 
     /**
      * Query a context for all the contexts within it that match a criteria.
-     * @param context The context to query.
+     *
+     * @param context  The context to query.
      * @param criteria The criteria to match.
      * @return the list of matches in ranked order
      */
     public static List<Context> query(Context context, Context criteria) {
-		List<Context> contexts = new LinkedList<>();
-		List<Ranked<Context>> results = rankedQuery(context, criteria);
-		for (Ranked<Context> ranked : results) {
-			contexts.add(ranked.getData());
-		}
-		return contexts;
-	}
+        List<Context> contexts = new LinkedList<>();
+        List<Ranked<Context>> results = rankedQuery(context, criteria);
+        for (Ranked<Context> ranked : results) {
+            contexts.add(ranked.getData());
+        }
+        return contexts;
+    }
 
     /**
      * Query a context for all the contexts within it that match a criteria.
-     * @param context The context to query
+     *
+     * @param context  The context to query
      * @param criteria the criteria to match
      * @return the results, with their associated ranks in ranked order
      */
-	public static List<Ranked<Context>> rankedQuery(Context context, Context criteria) {
-		List<Ranked<Context>> results = new LinkedList<>();
+    public static List<Ranked<Context>> rankedQuery(Context context, Context criteria) {
+        List<Ranked<Context>> results = new LinkedList<>();
 
-		if (context.isContainer()) {
-			for (Context child : context.getContained().values()) {
-				results.addAll(rankedQuery(child, criteria));
-			}
-		} else {
-			float rank = context.similarity(criteria);
-			if (rank > Similarity.NOT_SIMILAR) {
-				results.add(new Ranked<>(rank, context.getAge(), context));
-			}
-		}
+        if (context.isContainer()) {
+            for (Context child : context.getContained().values()) {
+                results.addAll(rankedQuery(child, criteria));
+            }
+        } else {
+            float rank = context.similarity(criteria);
+            if (rank > Similarity.NOT_SIMILAR) {
+                results.add(new Ranked<>(rank, context.getAge(), context));
+            }
+        }
 
-		Collections.sort(results);
-		return results;
-	}
+        Collections.sort(results);
+        return results;
+    }
 
     /**
      * Print out the contents of a database, flattening all the inheritance to the instance level.
-     * @param database the database
+     *
+     * @param database    the database
      * @param printStream the output stream
      */
-	public static void print(Database database, PrintStream printStream) {
-		List<Context> types = new LinkedList<>(database.getContained().values());
-		Collections.sort(types);
-		for (Context type : types) {
-			printStream.format("Type: %s\n", ((Type) type).getName());
-			List<Context> instances = new LinkedList<>(type.getContained().values());
-			Collections.sort(instances);
-			for (Context instance : instances) {
-				printStream.format("\tInstance: %s\n", ((Instance) instance).getName());
-				List<String> attrNames = new LinkedList<>(instance.keySet());
-				Collections.sort(attrNames);
-				for (String key : attrNames) {
-					printStream.format("\t\t%20s: %s\n", key, instance.get(key));
-				}
-			}
-		}
-	}
+    public static void print(Database database, PrintStream printStream) {
+        List<Context> types = new LinkedList<>(database.getContained().values());
+        Collections.sort(types);
+        for (Context type : types) {
+            printStream.format("Type: %s\n", ((Type) type).getName());
+            List<Context> instances = new LinkedList<>(type.getContained().values());
+            Collections.sort(instances);
+            for (Context instance : instances) {
+                printStream.format("\tInstance: %s\n", ((Instance) instance).getName());
+                List<String> attrNames = new LinkedList<>(instance.keySet());
+                Collections.sort(attrNames);
+                for (String key : attrNames) {
+                    printStream.format("\t\t%20s: %s\n", key, instance.get(key));
+                }
+            }
+        }
+    }
 
     /**
-     * Look up a resource as either a resource or a file.
+     * Look up a resource as either file or failing that a resource from the jar.
+     *
      * @param resource item name to look for
      * @return return input stream or null if not found
      */
     public static InputStream getResourceAsStream(String resource) {
-           InputStream stream = ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
-           if (stream != null) {
-               return stream;
-           }
+        try {
+            return new FileInputStream(resource);
+        } catch (FileNotFoundException e) { }
 
-           // Couldn't be pulled by the class loader, maybe it's just a file
-           try {
-               stream = new FileInputStream(resource);
-           } catch (FileNotFoundException e) {
-               LOGGER.warning("Failed to find " + resource + " as resource or file.");
-           }
-
-           return stream;
-       }
+        InputStream inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(resource);
+        if (inputStream == null) {
+            LOGGER.info("Failed to find " + resource + " as resource or file.");
+        }
+        return inputStream;
+    }
 }
