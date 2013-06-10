e2316c3f9693083dd8384425bd56fa706f73b2e0
nicholas.christopher <nicholas.christopher@seleritycorp.com>
2013-06-10 16:00:13 -0400

    Fixes import issue with file descriptors closing.

--- src/main/java/scotty/template/Parser.java
+++ src/main/java/scotty/template/Parser.java
@@ -32,113 +32,113 @@ import static scotty.template.Tokens.*;
  */
 public final class Parser {
 
-	private Parser() {
-	}
+    private Parser() {
+    }
 
-	public static void parse(InputStream inputStream, final OutputStream outputStream, final Database database,
-							 Context context) throws IOException, EvalError {
-		final Interpreter beanShell = new Interpreter();
+    public static void parse(final InputStream inputStream, final OutputStream outputStream, final Database database,
+                             final Context context) throws IOException, EvalError {
+        final Interpreter beanShell = new Interpreter();
 
-		try (PrintStream output = new PrintStream(outputStream)) {
+        PrintStream printStream = new PrintStream(outputStream);
 
-			beanShell.getNameSpace().importPackage(Database.class.getPackage().getName());
-			beanShell.set("database", database);
-			beanShell.set("context", context);
-			beanShell.set("output", output);
+        beanShell.getNameSpace().importPackage(Database.class.getPackage().getName());
+        beanShell.set("database", database);
+        beanShell.set("context", context);
+        beanShell.set("output", printStream);
 
-			while (true) {
-				if (!scanTo(Tokens.OPEN, inputStream, outputStream)) {
-					break;
-				}
+        while (true) {
+            if (!scanTo(Tokens.OPEN, inputStream, outputStream)) {
+                break;
+            }
 
-				ByteArrayOutputStream scriptOutput = new ByteArrayOutputStream();
-				scanTo(Tokens.CLOSE, inputStream, scriptOutput);
-				final String script = scriptOutput.toString();
-				final char operator = script.charAt(0);
-				final String scriptBody = script.substring(1).trim();
-				String value;
+            ByteArrayOutputStream scriptOutput = new ByteArrayOutputStream();
+            scanTo(Tokens.CLOSE, inputStream, scriptOutput);
+            final String script = scriptOutput.toString();
+            final char operator = script.charAt(0);
+            final String scriptBody = script.substring(1).trim();
+            String value;
 
-				switch (operator) {
-					case QUERY:
-						final Context queryContext;
-						final int endOfAttrName = scriptBody.indexOf(' ');
-						final String attributeName;
-						if (endOfAttrName == -1) {
-							attributeName = scriptBody;
-							queryContext = new Context(context);
-						} else {
-							attributeName = scriptBody.substring(0, endOfAttrName);
-							queryContext = new Context(context, scriptBody.substring(endOfAttrName));
-						}
-						List<Context> matches = database.query(queryContext);
-						if (matches.size() == 0) {
-							break;
-						}
-						value = matches.get(0).get(attributeName);
-						if (value != null) {
-							outputStream.write(value.getBytes());
-						}
-						break;
-					case IMPORT:
-						final Context importContext;
-						final int endOfFileName = scriptBody.indexOf(' ');
-						final String fileName;
-						if (endOfFileName == -1) {
-							fileName = scriptBody;
-							importContext = new Context(context);
-						} else {
-							fileName = scriptBody.substring(0, endOfFileName);
-							importContext = new Context(context, scriptBody.substring(endOfFileName));
-						}
-						parse(Utilities.getResourceAsStream(fileName), outputStream, database, importContext);
-						break;
-					case TYPES:
-						String[] types = scriptBody.split(",");
-						for (String type : types) {
-							if (!database.getContained().containsKey(type.trim())) {
-								throw new IllegalStateException("Database lacks required type: " + type);
-							}
-						}
-						break;
-					case IN_CONTEXT:
-						String[] keys = scriptBody.split(",");
-						for (String key : keys) {
-							if (!context.getMap().containsKey(key.trim())) {
-								throw new IllegalStateException("Runtime context lacks required attribute: " + key);
-							}
-						}
-						break;
-					case ' ':
-					case '\t':
-					case '\n':
-					case '\r':
-						beanShell.setOut(output);
-						beanShell.eval(scriptBody);
-						break;
+            switch (operator) {
+                case QUERY:
+                    final Context queryContext;
+                    final int endOfAttrName = scriptBody.indexOf(' ');
+                    final String attributeName;
+                    if (endOfAttrName == -1) {
+                        attributeName = scriptBody;
+                        queryContext = new Context(context);
+                    } else {
+                        attributeName = scriptBody.substring(0, endOfAttrName);
+                        queryContext = new Context(context, scriptBody.substring(endOfAttrName));
+                    }
+                    List<Context> matches = database.query(queryContext);
+                    if (matches.size() == 0) {
+                        break;
+                    }
+                    value = matches.get(0).get(attributeName);
+                    if (value != null) {
+                        outputStream.write(value.getBytes());
+                    }
+                    break;
+                case IMPORT:
+                    final Context importContext;
+                    final int endOfFileName = scriptBody.indexOf(' ');
+                    final String fileName;
+                    if (endOfFileName == -1) {
+                        fileName = scriptBody;
+                        importContext = new Context(context);
+                    } else {
+                        fileName = scriptBody.substring(0, endOfFileName);
+                        importContext = new Context(context, scriptBody.substring(endOfFileName));
+                    }
+                    parse(Utilities.getResourceAsStream(fileName), outputStream, database, importContext);
+                    break;
+                case TYPES:
+                    String[] types = scriptBody.split(",");
+                    for (String type : types) {
+                        if (!database.getContained().containsKey(type.trim())) {
+                            throw new IllegalStateException("Database lacks required type: " + type);
+                        }
+                    }
+                    break;
+                case IN_CONTEXT:
+                    String[] keys = scriptBody.split(",");
+                    for (String key : keys) {
+                        if (!context.containsKey(key.trim())) {
+                            throw new IllegalStateException("Runtime context lacks required attribute: " + key);
+                        }
+                    }
+                    break;
+                case ' ':
+                case '\t':
+                case '\n':
+                case '\r':
+                    beanShell.setOut(printStream);
+                    beanShell.eval(scriptBody);
+                    break;
 
-					default:
-						break;
-				}
-			}
+                default:
+                    break;
+            }
+        }
 
-			outputStream.flush();
-		}
-	}
+        printStream.flush();
+        outputStream.flush();
+    }
 
-	private static boolean scanTo(String patternStr, InputStream inputStream, OutputStream outputStream) throws IOException {
-		final CharQueue pattern = new CharQueue(patternStr, null);
-		final CharQueue buffer = new CharQueue(patternStr.length(), outputStream);
+    private static boolean scanTo(String patternStr, InputStream inputStream, OutputStream outputStream) throws IOException {
+        final CharQueue pattern = new CharQueue(patternStr, null);
+        final CharQueue buffer = new CharQueue(patternStr.length(), outputStream);
 
-		int ch;
-		while ((ch = inputStream.read()) > -1) {
-			buffer.add((char) ch);
-			if (pattern.compareTo(buffer) == 0) {
-				return true;
-			}
-		}
+        int ch;
+        while ((ch = inputStream.read()) > -1) {
+            buffer.add((char) ch);
+            if (pattern.compareTo(buffer) == 0) {
+                return true;
+            }
+        }
 
-		outputStream.write(buffer.toString().getBytes());
-		return false;
-	}
+        outputStream.write(buffer.toString().getBytes());
+        return false;
+    }
 
 }
