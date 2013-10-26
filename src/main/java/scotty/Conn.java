34eadf7359e86949324b18e74cf2980f65cc4bb4
Nicholas Christopher <nwillc@gmail.com>
2013-10-26 13:53:47 -0400

    Cleaning file management

--- src/main/java/scotty/Conn.java
+++ src/main/java/scotty/Conn.java
@@ -21,9 +21,7 @@ import scotty.database.Database;
 import scotty.template.NamedScriptEngine;
 
 import javax.script.ScriptException;
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,150 +35,150 @@ import static scotty.template.Parser.parse;
  * Scotty's conn, where it all gets put together driven by command line.
  */
 public final class Conn {
-    private static final Logger LOGGER = Logger.getLogger(Conn.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(Conn.class.getName());
 
-    private Conn() {
-    }
+	private Conn() {
+	}
 
-    public static void main(String[] args) {
-        CommandLineParser parser = new PosixParser();
-        Options options = setupOptions();
-        String templateFile;
-        String scriptEngineName = NamedScriptEngine.DEFAULT_LANGUAGE;
+	public static void main(String[] args) {
+		CommandLineParser parser = new PosixParser();
+		Options options = setupOptions();
+		String templateFile;
+		String scriptEngineName = NamedScriptEngine.DEFAULT_LANGUAGE;
 
-        try {
-            CommandLine commandLine = parser.parse(options, args);
+		try {
+			CommandLine commandLine = parser.parse(options, args);
 
-            if (commandLine.hasOption(HELP)) {
-                help(options, 0);
-            }
+			if (commandLine.hasOption(HELP)) {
+				help(options, 0);
+			}
 
-            if (!commandLine.hasOption(DATABASE)) {
-                LOGGER.severe("A database must be defined");
-                help(options, 3);
-            }
+			if (!commandLine.hasOption(DATABASE)) {
+				LOGGER.severe("A database must be defined");
+				help(options, 3);
+			}
 
-            String[] databaseFiles = commandLine.getOptionValue(DATABASE).split(",");
-            Database database = Database.parse(databaseFiles);
+			String[] databaseFiles = commandLine.getOptionValue(DATABASE).split(",");
+			Database database = Database.parse(databaseFiles);
 
-            if (commandLine.hasOption(FOLDER)) {
-                final String outputFolder = commandLine.getOptionValue(FOLDER);
-                if ("-".equals(outputFolder)) {
-                    database.put(FOLDER, outputFolder);
-                } else {
-                    final File file = new File(outputFolder);
+			if (commandLine.hasOption(FOLDER)) {
+				final String outputFolder = commandLine.getOptionValue(FOLDER);
+				if ("-".equals(outputFolder)) {
+					database.put(FOLDER, outputFolder);
+				} else {
+					final File file = new File(outputFolder);
 
-                    if (!(file.exists() && file.isDirectory() && file.canWrite())) {
-                        LOGGER.severe("Folder must exist, be a directory and be writable.");
-                        help(options, 4);
-                    }
-                    database.put(FOLDER, file.getAbsolutePath());
-                }
-            }
+					if (!(file.exists() && file.isDirectory() && file.canWrite())) {
+						LOGGER.severe("Folder must exist, be a directory and be writable.");
+						help(options, 4);
+					}
+					database.put(FOLDER, file.getAbsolutePath());
+				}
+			}
 
-            if (commandLine.hasOption(PRINT)) {
-                print(database, System.out);
-                System.exit(0);
-            }
+			if (commandLine.hasOption(PRINT)) {
+				print(database, System.out);
+				System.exit(0);
+			}
 
-            InputStream inputStream;
-            if (commandLine.hasOption(TEMPLATE)) {
-                templateFile = commandLine.getOptionValue(TEMPLATE);
-                inputStream = getResourceAsStream(templateFile);
-            } else {
-                templateFile = "stdin";
-                inputStream = System.in;
-            }
+			InputStream inputStream;
+			if (commandLine.hasOption(TEMPLATE)) {
+				templateFile = commandLine.getOptionValue(TEMPLATE);
+				inputStream = getResourceAsStream(templateFile);
+			} else {
+				templateFile = "stdin";
+				inputStream = System.in;
+			}
 
-            final OutputStream outputStream;
-            if (commandLine.hasOption(OUTPUT)) {
-                outputStream = getPath(database.get(FOLDER), commandLine.getOptionValue(OUTPUT));
-            } else {
-                outputStream = System.out;
-            }
+			final OutputStream outputStream;
+			if (commandLine.hasOption(OUTPUT)) {
+				outputStream = getPath(database.get(FOLDER), commandLine.getOptionValue(OUTPUT));
+			} else {
+				outputStream = new FileOutputStream(FileDescriptor.out);
+			}
 
-            Context context;
-            if (commandLine.hasOption(CONTEXT)) {
-                context = new Context(commandLine.getOptionValue(CONTEXT));
-            } else {
-                context = new Context();
-            }
+			Context context;
+			if (commandLine.hasOption(CONTEXT)) {
+				context = new Context(commandLine.getOptionValue(CONTEXT));
+			} else {
+				context = new Context();
+			}
 
-            if (commandLine.hasOption(LANGUAGE)) {
-                scriptEngineName = commandLine.getOptionValue(LANGUAGE);
-            }
+			if (commandLine.hasOption(LANGUAGE)) {
+				scriptEngineName = commandLine.getOptionValue(LANGUAGE);
+			}
 
-            NamedScriptEngine scriptEngine = new NamedScriptEngine(scriptEngineName, templateFile);
-            parse(inputStream, outputStream, database, context, scriptEngine);
-        } catch (ScriptException evalError) {
-            LOGGER.severe("Exit on script error.");
-            System.exit(2);
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, e.toString(), e);
-            System.exit(3);
-        }
-    }
+			NamedScriptEngine scriptEngine = new NamedScriptEngine(scriptEngineName, templateFile);
+			parse(inputStream, outputStream, database, context, scriptEngine);
+		} catch (ScriptException evalError) {
+			LOGGER.severe("Exit on script error.");
+			System.exit(2);
+		} catch (Exception e) {
+			LOGGER.log(Level.SEVERE, e.toString(), e);
+			System.exit(3);
+		}
+	}
 
-    private static void help(Options options, int status) {
-        HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp("java -jar scotty.jar [options]", options);
-        System.exit(status);
-    }
+	private static void help(Options options, int status) {
+		HelpFormatter formatter = new HelpFormatter();
+		formatter.printHelp("java -jar scotty.jar [options]", options);
+		System.exit(status);
+	}
 
-    private static Options setupOptions() {
-        Option option;
+	private static Options setupOptions() {
+		Option option;
 
-        Options options = new Options();
+		Options options = new Options();
 
-        option = new Option(HELP.substring(0, 1), HELP, false, "print this message.");
-        options.addOption(option);
+		option = new Option(HELP.substring(0, 1), HELP, false, "print this message.");
+		options.addOption(option);
 
-        option = new Option(DATABASE.substring(0, 1), DATABASE, true,
-                "Comma separated of XML files containing types in database.");
-        option.setArgName("filenames");
-        option.setArgs(1);
-        option.setRequired(false);
-        options.addOption(option);
+		option = new Option(DATABASE.substring(0, 1), DATABASE, true,
+				"Comma separated of XML files containing types in database.");
+		option.setArgName("filenames");
+		option.setArgs(1);
+		option.setRequired(false);
+		options.addOption(option);
 
-        option = new Option(CONTEXT.substring(0, 1), CONTEXT, true,
-                "Comma separated list of assignments to act as the context.");
-        option.setArgName("assignments");
-        option.setArgs(1);
-        options.addOption(option);
+		option = new Option(CONTEXT.substring(0, 1), CONTEXT, true,
+				"Comma separated list of assignments to act as the context.");
+		option.setArgName("assignments");
+		option.setArgs(1);
+		options.addOption(option);
 
-        option = new Option(TEMPLATE.substring(0, 1), TEMPLATE, true,
-                "The name of the template file.");
-        option.setArgName("filename");
-        option.setArgs(1);
-        option.setRequired(false);
-        options.addOption(option);
+		option = new Option(TEMPLATE.substring(0, 1), TEMPLATE, true,
+				"The name of the template file.");
+		option.setArgName("filename");
+		option.setArgs(1);
+		option.setRequired(false);
+		options.addOption(option);
 
-        option = new Option(OUTPUT.substring(0, 1), OUTPUT, true,
-                "The name of the output file.");
-        option.setRequired(false);
-        option.setArgName("filename");
-        option.setArgs(1);
-        options.addOption(option);
+		option = new Option(OUTPUT.substring(0, 1), OUTPUT, true,
+				"The name of the output file.");
+		option.setRequired(false);
+		option.setArgName("filename");
+		option.setArgs(1);
+		options.addOption(option);
 
-        option = new Option(FOLDER.substring(0, 1), FOLDER, true,
-                "The folder to put output files in. Use \"-\" for standard out.");
-        option.setRequired(false);
-        option.setArgName("directory");
-        option.setArgs(1);
-        options.addOption(option);
+		option = new Option(FOLDER.substring(0, 1), FOLDER, true,
+				"The folder to put output files in. Use \"-\" for standard out.");
+		option.setRequired(false);
+		option.setArgName("directory");
+		option.setArgs(1);
+		options.addOption(option);
 
-        option = new Option(PRINT.substring(0, 1), PRINT, false,
-                "Print the database out as a flattened text file.");
-        option.setRequired(false);
-        options.addOption(option);
+		option = new Option(PRINT.substring(0, 1), PRINT, false,
+				"Print the database out as a flattened text file.");
+		option.setRequired(false);
+		options.addOption(option);
 
-        option = new Option(LANGUAGE.substring(0, 1), LANGUAGE, true,
-                "Define an alternative scripting language. BeanShell used by default.");
-        option.setRequired(false);
-        option.setArgName("language");
-        option.setArgs(1);
-        options.addOption(option);
+		option = new Option(LANGUAGE.substring(0, 1), LANGUAGE, true,
+				"Define an alternative scripting language. BeanShell used by default.");
+		option.setRequired(false);
+		option.setArgName("language");
+		option.setArgs(1);
+		options.addOption(option);
 
-        return options;
-    }
+		return options;
+	}
 }
--- src/main/java/scotty/template/Parser.java
+++ src/main/java/scotty/template/Parser.java
@@ -33,152 +33,153 @@ import static scotty.template.Tokens.*;
  * output.
  */
 public final class Parser {
-    private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
+	private static final Logger LOGGER = Logger.getLogger(Parser.class.getName());
 
-    private Parser() {
-    }
+	private Parser() {
+	}
 
-    /**
-     * Parse a template, handling the annotations it contains, employing a database and runtime context.
-     *
-     * @param inputStream  the input stream of the template.
-     * @param outputStream where to send the output.
-     * @param database     the database to run queries against
-     * @param context      the runtime context of assignments
-     * @param scriptEngine the script engine being used
-     * @throws IOException     if one of the files can not be read.
-     * @throws ScriptException if there is a syntax error in the bean shell
-     */
-    public static void parse(final InputStream inputStream, OutputStream outputStream, final Database database,
-                             final Context context, NamedScriptEngine scriptEngine) throws IOException, ScriptException {
-        export(scriptEngine, database, context, outputStream);
-        while (true) {
-            if (!scanTo(Tokens.OPEN, inputStream, outputStream)) {
-                break;
-            }
+	/**
+	 * Parse a template, handling the annotations it contains, employing a database and runtime context.
+	 *
+	 * @param inputStream  the input stream of the template.
+	 * @param outputStream where to send the output.
+	 * @param database     the database to run queries against
+	 * @param context      the runtime context of assignments
+	 * @param scriptEngine the script engine being used
+	 * @throws IOException     if one of the files can not be read.
+	 * @throws ScriptException if there is a syntax error in the bean shell
+	 */
+	public static void parse(final InputStream inputStream, OutputStream outputStream, final Database database,
+							 final Context context, NamedScriptEngine scriptEngine) throws IOException, ScriptException {
+		export(scriptEngine, database, context, outputStream);
+		while (true) {
+			if (!scanTo(Tokens.OPEN, inputStream, outputStream)) {
+				break;
+			}
 
-            ByteArrayOutputStream scriptOutput = new ByteArrayOutputStream();
-            if (!scanTo(Tokens.CLOSE, inputStream, scriptOutput)) {
-                throw new IllegalStateException("Unclosed Scotty markup.");
-            }
-            final String script = scriptOutput.toString();
-            final char operator = script.charAt(0);
-            final String body = script.substring(1).trim();
-            String value;
+			ByteArrayOutputStream scriptOutput = new ByteArrayOutputStream();
+			if (!scanTo(Tokens.CLOSE, inputStream, scriptOutput)) {
+				throw new IllegalStateException("Unclosed Scotty markup.");
+			}
+			final String script = scriptOutput.toString();
+			final char operator = script.charAt(0);
+			final String body = script.substring(1).trim();
+			String value;
 
-            switch (operator) {
-                case CONTEXT:
-                    outputStream.write(context.get(body, "").getBytes());
-                    break;
-                case QUERY:
-                    final Context queryContext;
-                    final int endOfAttrName = body.indexOf(' ');
-                    final String attributeName;
-                    if (endOfAttrName == -1) {
-                        attributeName = body;
-                        queryContext = new Context(context);
-                    } else {
-                        attributeName = body.substring(0, endOfAttrName);
-                        queryContext = new Context(context, body.substring(endOfAttrName));
-                    }
-                    List<Context> matches = database.query(queryContext);
-                    if (matches.size() == 0) {
-                        break;
-                    }
-                    value = matches.get(0).get(attributeName);
-                    if (value != null) {
-                        outputStream.write(value.getBytes());
-                    }
-                    break;
-                case IMPORT:
-                    final Context importContext;
-                    final int endOfFileName = body.indexOf(' ');
-                    final String fileName;
-                    if (endOfFileName == -1) {
-                        fileName = body;
-                        importContext = new Context(context);
-                    } else {
-                        fileName = body.substring(0, endOfFileName);
-                        importContext = new Context(context, body.substring(endOfFileName));
-                    }
-                    NamedScriptEngine newScriptEngine = new NamedScriptEngine(scriptEngine.getLanguageName(), fileName);
-                    parse(getResourceAsStream(fileName), outputStream, database, importContext, newScriptEngine);
-                    break;
-                case TYPES:
-                    String[] types = body.split(",");
-                    for (String type : types) {
-                        if (!database.getContained().containsKey(type.trim())) {
-                            throw new IllegalStateException("Database lacks required type: " + type);
-                        }
-                    }
-                    break;
-                case LANGUAGE:
-                    scriptEngine = new NamedScriptEngine(body, scriptEngine.getScriptName());
-                    export(scriptEngine, database, context, outputStream);
-                    break;
-                case OUTPUT:
-                    outputStream.flush();
-                    outputStream = getPath(database.get(Cli.FOLDER), body);
-                    export(scriptEngine, database, context, outputStream);
-                    break;
-                case IN_CONTEXT:
-                    String[] keys = body.split(",");
-                    for (String key : keys) {
-                        if (!context.containsKey(key.trim())) {
-                            throw new IllegalStateException("Runtime context lacks required attribute: " + key);
-                        }
-                    }
-                    break;
-                case ' ':
-                case '\t':
-                case '\n':
-                case '\r':
-                    try {
-                        scriptEngine.eval(body);
-                    } catch (ScriptException scriptException) {
-                        LOGGER.severe(scriptException.getFileName() + ": [" + scriptException.getLineNumber() + "] " + scriptException.getMessage());
-                        throw scriptException;
-                    }
-                    break;
-            }
-        }
+			switch (operator) {
+				case CONTEXT:
+					outputStream.write(context.get(body, "").getBytes());
+					break;
+				case QUERY:
+					final Context queryContext;
+					final int endOfAttrName = body.indexOf(' ');
+					final String attributeName;
+					if (endOfAttrName == -1) {
+						attributeName = body;
+						queryContext = new Context(context);
+					} else {
+						attributeName = body.substring(0, endOfAttrName);
+						queryContext = new Context(context, body.substring(endOfAttrName));
+					}
+					List<Context> matches = database.query(queryContext);
+					if (matches.size() == 0) {
+						break;
+					}
+					value = matches.get(0).get(attributeName);
+					if (value != null) {
+						outputStream.write(value.getBytes());
+					}
+					break;
+				case IMPORT:
+					final Context importContext;
+					final int endOfFileName = body.indexOf(' ');
+					final String fileName;
+					if (endOfFileName == -1) {
+						fileName = body;
+						importContext = new Context(context);
+					} else {
+						fileName = body.substring(0, endOfFileName);
+						importContext = new Context(context, body.substring(endOfFileName));
+					}
+					NamedScriptEngine newScriptEngine = new NamedScriptEngine(scriptEngine.getLanguageName(), fileName);
+					parse(getResourceAsStream(fileName), outputStream, database, importContext, newScriptEngine);
+					break;
+				case TYPES:
+					String[] types = body.split(",");
+					for (String type : types) {
+						if (!database.getContained().containsKey(type.trim())) {
+							throw new IllegalStateException("Database lacks required type: " + type);
+						}
+					}
+					break;
+				case LANGUAGE:
+					scriptEngine = new NamedScriptEngine(body, scriptEngine.getScriptName());
+					export(scriptEngine, database, context, outputStream);
+					break;
+				case OUTPUT:
+					outputStream.flush();
+					outputStream.close();
+					outputStream = getPath(database.get(Cli.FOLDER), body);
+					export(scriptEngine, database, context, outputStream);
+					break;
+				case IN_CONTEXT:
+					String[] keys = body.split(",");
+					for (String key : keys) {
+						if (!context.containsKey(key.trim())) {
+							throw new IllegalStateException("Runtime context lacks required attribute: " + key);
+						}
+					}
+					break;
+				case ' ':
+				case '\t':
+				case '\n':
+				case '\r':
+					try {
+						scriptEngine.eval(body);
+					} catch (ScriptException scriptException) {
+						LOGGER.severe(scriptException.getFileName() + ": [" + scriptException.getLineNumber() + "] " + scriptException.getMessage());
+						throw scriptException;
+					}
+					break;
+			}
+		}
 
-        outputStream.flush();
-    }
+		outputStream.flush();
+	}
 
-    /**
-     * Scan for a pattern in a stream, shunting data that is passed by to an output stream.
-     *
-     * @param patternStr   the pattern to search in
-     * @param inputStream  the stream to read from
-     * @param outputStream the stream to send data we have passed to
-     * @return if the there was a match
-     * @throws IOException
-     */
-    private static boolean scanTo(String patternStr, InputStream inputStream, OutputStream outputStream) throws IOException {
-        final CharQueue pattern = new CharQueue(patternStr, null);
-        final CharQueue buffer = new CharQueue(patternStr.length(), outputStream);
+	/**
+	 * Scan for a pattern in a stream, shunting data that is passed by to an output stream.
+	 *
+	 * @param patternStr   the pattern to search in
+	 * @param inputStream  the stream to read from
+	 * @param outputStream the stream to send data we have passed to
+	 * @return if the there was a match
+	 * @throws IOException
+	 */
+	private static boolean scanTo(String patternStr, InputStream inputStream, OutputStream outputStream) throws IOException {
+		final CharQueue pattern = new CharQueue(patternStr, null);
+		final CharQueue buffer = new CharQueue(patternStr.length(), outputStream);
 
-        int ch;
-        while ((ch = inputStream.read()) > -1) {
-            buffer.add((char) ch);
-            if (pattern.compareTo(buffer) == 0) {
-                return true;
-            }
-        }
+		int ch;
+		while ((ch = inputStream.read()) > -1) {
+			buffer.add((char) ch);
+			if (pattern.compareTo(buffer) == 0) {
+				return true;
+			}
+		}
 
-        outputStream.write(buffer.toString().getBytes());
-        return false;
-    }
+		outputStream.write(buffer.toString().getBytes());
+		return false;
+	}
 
-    private static void export(NamedScriptEngine scriptEngine, Database database, Context context, OutputStream outputStream) throws ScriptException {
-        try {
-            scriptEngine.put("database", database);
-            scriptEngine.put("context", context);
-            scriptEngine.put("output", new PrintStream(outputStream));
-        } catch (ScriptException scriptException) {
-            LOGGER.severe(scriptException.toString());
-            throw scriptException;
-        }
-    }
+	private static void export(NamedScriptEngine scriptEngine, Database database, Context context, OutputStream outputStream) throws ScriptException {
+		try {
+			scriptEngine.put("database", database);
+			scriptEngine.put("context", context);
+			scriptEngine.put("output", new PrintStream(outputStream));
+		} catch (ScriptException scriptException) {
+			LOGGER.severe(scriptException.toString());
+			throw scriptException;
+		}
+	}
 }
