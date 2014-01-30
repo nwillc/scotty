81219f139970c57016fa07976f8db0c393282f56
Nicholas Christopher <nwillc@gmail.com>
2014-01-30 07:41:04 -0500

    Iterable fix

--- src/main/java/scotty/util/Iterables.java
+++ src/main/java/scotty/util/Iterables.java
@@ -1,6 +1,7 @@
 package scotty.util;
 
 import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
 
 import static com.google.common.collect.Iterables.transform;
 
@@ -8,13 +9,12 @@ import static com.google.common.collect.Iterables.transform;
  *
  */
 public class Iterables {
-	public static <T> void forEach(Iterable i, final Consumer<T> c) {
-	  transform(i, new Function<T,Void>() {
-		  @Override
-		  public Void apply(T t) {
-			  c.accept(t);
-			  return null;
-		  }
-	  });
+	public static <T> void forEach(Iterable<T> i, final Consumer<T> c) {
+		Preconditions.checkNotNull(i);
+		Preconditions.checkNotNull(c);
+
+		for (T t : i) {
+			c.accept(t);
+		}
 	}
 }
