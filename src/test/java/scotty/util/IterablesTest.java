9cf5d2187fa7b3f331c0de9756153eb60a219823
Nicholas Christopher <nwillc@gmail.com>
2014-02-01 13:29:27 -0500

    Iterables tests

---
+++ src/test/java/scotty/util/IterablesTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2013-2014, nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or
+ * without fee is hereby granted, provided that the above copyright notice and this permission
+ * notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO
+ * THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT
+ * SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR
+ * ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF
+ * CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+ * OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package scotty.util;
+
+import com.google.common.base.Function;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static junit.framework.Assert.assertEquals;
+import static scotty.util.Iterables.forEach;
+
+/**
+ *
+ */
+public class IterablesTest {
+
+	private List<String> strings;
+
+	@Before
+	public void setUp() throws Exception {
+		strings = newArrayList("a", "b", "c");
+	}
+
+	@Test
+	public void shouldForEachConsumer() throws Exception {
+		final StringBuffer stringBuffer = new StringBuffer();
+		forEach(strings, new Consumer<String>() {
+			@Override
+			public void accept(String s) {
+				stringBuffer.append(s);
+			}
+		});
+		assertEquals("abc", stringBuffer.toString());
+	}
+
+	@Test
+	public void shouldForEachFunction() throws Exception {
+		Function<String,String> function = new Function<String, String>() {
+			private StringBuffer stringBuffer = new StringBuffer();
+			@Override
+			public String apply(String s) {
+				stringBuffer.append(s);
+				return stringBuffer.toString();
+			}
+		};
+		assertEquals("abc", forEach(strings,function));
+	}
+}
