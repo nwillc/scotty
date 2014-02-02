405c9ddc4812252d394a6af0bb81644723c04d64
Nicholas Christopher <nwillc@gmail.com>
2014-02-02 14:04:40 -0500

    Refactored out missused functions and code cleaning

--- src/main/java/scotty/database/Database.java
+++ src/main/java/scotty/database/Database.java
@@ -19,6 +19,7 @@ import com.google.common.base.Function;
 import scotty.database.parser.DbParserUtilities;
 import scotty.database.parser.Parser;
 import scotty.util.Consumer;
+import scotty.util.Optionals;
 
 import java.io.InputStream;
 import java.io.PrintStream;
@@ -34,122 +35,119 @@ import static scotty.util.Iterables.forEach;
  */
 public class Database extends Context {
 
-    /**
-     * Parse a collection of streams open to Type data to create a Database.
-     *
-     * @param inputStreams Collection of input streams
-     * @return Database of types
-     */
-    public static Database parse(InputStream ... inputStreams) {
-        final Database database = new Database();
-        forEach(newArrayIterable(inputStreams), new Consumer<InputStream>() {
-            @Override
-            public void accept(InputStream stream) {
-                Parser.parse(stream).transform(new Function<Type, Object>() {
-                    @Override
-                    public Object apply(Type type) {
-                        database.getContained().put(type.getName(), type);
-                        return this;
-                    }
-                });
-            }
-        });
-        return database;
-    }
+	/**
+	 * Parse a collection of streams open to Type data to create a Database.
+	 *
+	 * @param inputStreams Collection of input streams
+	 * @return Database of types
+	 */
+	public static Database parse(InputStream... inputStreams) {
+		final Database database = new Database();
+		forEach(newArrayIterable(inputStreams), new Consumer<InputStream>() {
+			@Override
+			public void accept(InputStream stream) {
+				Optionals.accept(Parser.parse(stream), new Consumer<Type>() {
+					@Override
+					public void accept(Type type) {
+						database.getContained().put(type.getName(), type);
+					}
+				});
+			}
+		});
+		return database;
+	}
 
-    /**
-     * Parse a collection of files containing Type data to create a Database.
-     *
-     * @param files collection of files
-     * @return Database of types
-    */
-    public static Database parse(String... files) {
-        final Database database = new Database();
-        forEach(newArrayIterable(files), new Consumer<String>() {
-            @Override
-            public void accept(String file) {
-                Parser.parse(file).transform(new Function<Type, Object>() {
-                    @Override
-                    public Object apply(Type type) {
-                        database.getContained().put(type.getName(), type);
-                        return this;
-                    }
-                });
-            }
-        });
-        return database;
-    }
+	/**
+	 * Parse a collection of files containing Type data to create a Database.
+	 *
+	 * @param files collection of files
+	 * @return Database of types
+	 */
+	public static Database parse(String... files) {
+		final Database database = new Database();
+		forEach(newArrayIterable(files), new Consumer<String>() {
+			@Override
+			public void accept(String file) {
+				Optionals.accept(Parser.parse(file), new Consumer<Type>() {
+					@Override
+					public void accept(Type type) {
+						database.getContained().put(type.getName(), type);
+					}
+				});
+			}
+		});
+		return database;
+	}
 
-    /**
-     * Return a list of instances matching a given set of criteria.
-     *
-     * @param criteria the criteria to match
-     * @return a list of instances in match rank order
-     */
-    public List<Context> query(String criteria) {
-        return query(new Context(criteria));
-    }
+	/**
+	 * Return a list of instances matching a given set of criteria.
+	 *
+	 * @param criteria the criteria to match
+	 * @return a list of instances in match rank order
+	 */
+	public List<Context> query(String criteria) {
+		return query(new Context(criteria));
+	}
 
-    /**
-     * Return a list of instances matching a given set of criteria.
-     *
-     * @param criteria the criteria to match
-     * @return a list of instances in match rank order
-     */
-    public List<Context> query(Context criteria) {
-        return DbParserUtilities.query(this, criteria);
-    }
+	/**
+	 * Return a list of instances matching a given set of criteria.
+	 *
+	 * @param criteria the criteria to match
+	 * @return a list of instances in match rank order
+	 */
+	public List<Context> query(Context criteria) {
+		return DbParserUtilities.query(this, criteria);
+	}
 
-    /**
-     * Return a set, in sorted order, of the values of a specified attribute of all the instances
-     * matching a criteria.
-     *
-     * @param criteria the criteria
-     * @param attr     the attribute name
-     * @return sorted set of values
-     */
-    public Set<String> query(String criteria, String attr) {
-        return query(new Context(criteria), attr);
-    }
+	/**
+	 * Return a set, in sorted order, of the values of a specified attribute of all the instances
+	 * matching a criteria.
+	 *
+	 * @param criteria the criteria
+	 * @param attr     the attribute name
+	 * @return sorted set of values
+	 */
+	public Set<String> query(String criteria, String attr) {
+		return query(new Context(criteria), attr);
+	}
 
-    /**
-     * Return a set, in sorted order, of the values of a specified attribute of all the instances
-     * matching a criteria.
-     *
-     * @param criteria the criteria
-     * @param attr     the attribute name
-     * @return sorted set of values
-     */
-    public Set<String> query(final Context criteria, final String attr) {
-        final Set<String> attributeValues = new TreeSet<>();
-        forEach(query(criteria), new Consumer<Context>() {
-            @Override
-            public void accept(Context context) {
-                context.getValue(attr).transform(new Function<Value, Object>() {
-                    @Override
-                    public Object apply(Value value) {
-                        forEach(value.values(), new Consumer<String>() {
-                            @Override
-                            public void accept(String s) {
-                                attributeValues.add(s);
-                            }
-                        });
-                        return this;
-                    }
-                });
-            }
-        });
-        return attributeValues;
-    }
+	/**
+	 * Return a set, in sorted order, of the values of a specified attribute of all the instances
+	 * matching a criteria.
+	 *
+	 * @param criteria the criteria
+	 * @param attr     the attribute name
+	 * @return sorted set of values
+	 */
+	public Set<String> query(final Context criteria, final String attr) {
+		final Set<String> attributeValues = new TreeSet<>();
+		forEach(query(criteria), new Consumer<Context>() {
+			@Override
+			public void accept(Context context) {
+				Optionals.accept(context.getValue(attr), new Consumer<Value>() {
+					@Override
+					public void accept(Value value) {
+						forEach(value.values(), new Consumer<String>() {
+							@Override
+							public void accept(String s) {
+								attributeValues.add(s);
+							}
+						});
+					}
+				});
+			}
+		});
+		return attributeValues;
+	}
 
-    /**
-     * Print out the contents of a database, flattening all the inheritance to the instance level and
-     * showing the similarity score with the context passed in.
-     *
-     * @param printStream the output stream
-     * @param context     the Context to compare instances to.
-     */
-    public void print(final PrintStream printStream, Context context) {
-        DbParserUtilities.print(this, printStream, context);
-    }
+	/**
+	 * Print out the contents of a database, flattening all the inheritance to the instance level and
+	 * showing the similarity score with the context passed in.
+	 *
+	 * @param printStream the output stream
+	 * @param context     the Context to compare instances to.
+	 */
+	public void print(final PrintStream printStream, Context context) {
+		DbParserUtilities.print(this, printStream, context);
+	}
 }
--- src/main/java/scotty/util/Optionals.java
+++ src/main/java/scotty/util/Optionals.java
@@ -15,30 +15,14 @@
 
 package scotty.util;
 
-import com.google.common.base.Function;
-import com.google.common.base.Preconditions;
+import com.google.common.base.Optional;
 
-public class Iterables {
-	private Iterables() {}
+public class Optionals {
+	private Optionals() {}
 
-	public static <T> void forEach(Iterable<T> i, final Consumer<T> c) {
-		Preconditions.checkNotNull(i);
-		Preconditions.checkNotNull(c);
-
-		for (T t : i) {
-			c.accept(t);
+	public static <T> void accept(Optional<T> optional, Consumer<T> consumer) {
+		if (optional.isPresent()) {
+			consumer.accept(optional.get());
 		}
-	}
-
-	public static <T,R> R forEach(Iterable<T> i, final Function<T,R> f) {
-		Preconditions.checkNotNull(i);
-		Preconditions.checkNotNull(f);
-		R ret = null;
-
-		for (T t : i) {
-			ret = f.apply(t);
-		}
-		Preconditions.checkNotNull(ret, "Function can not return null");
-		return ret;
 	}
 }
