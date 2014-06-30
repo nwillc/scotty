cacc42a019068fc14252a252d72683b89324e78e
Nicholas Christopher <nwillc@gmail.com>
2014-06-29 23:09:01 -0400

    Cucumber in gradle

--- src/test/java/features/Features.java
+++ src/test/java/features/Features.java
@@ -15,11 +15,11 @@
 
 package features;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"progress", "html:target/cucumber"})
 public class Features {
 }
 
